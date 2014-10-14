b8df107c00032f07b28ccdf647b33c37994c1384
Hudo <hudo.hudoletnjak@paddypower.com>
2014-10-14 16:21:56 +0100

    Selenium chrome driver test

---
+++ ScraperTryout/Scraper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chromeDriver = new ChromeDriver("drivers");
+            
+            chromeDriver.Navigate().GoToUrl("http://www.gsmarena.com");
+
+            var driverWait = new WebDriverWait(chromeDriver, new TimeSpan(0, 0, 10));
+
+            driverWait.Until(ExpectedConditions.ElementExists(By.Id("brandmenu")));
+
+            var brands = chromeDriver.FindElementsByCssSelector("div#brandmenu a");
+
+            Console.WriteLine("Brands: " + brands.Count);
+
+            foreach (var brand in brands)
+            {
+                Console.WriteLine(brand.Text);
+            }
+
+            Console.ReadKey();
+
+            chromeDriver.Close();
+            chromeDriver.Dispose();
+        }
+    }
+}
