e7205b4ee08bb3e9dbd3d12f8192ebadf535ad9e
Hudo <hudo.hudoletnjak@paddypower.com>
2015-01-05 12:17:54 +0000

    Sample EF one to one application created.

---
+++ EFone2one/EFone2one/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace EFone2one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlways<PeopleContext>());
+            
+            var context = new PeopleContext();
+            context.Database.Log = Console.WriteLine;
+
+            AddOnePerson(context);
+
+            var people = context.Persons.Include(x => x.Address).ToList();
+            Console.WriteLine("We have " + people.Count + " people in db.");
+
+            Console.WriteLine("done.");
+            Console.ReadKey();
+        }
+
+        private static void AddOnePerson(PeopleContext context)
+        {
+            var john = new Person {Name = "john"};
+            context.Persons.Add(john);
+            context.SaveChanges();
+
+            var address = new Address() {StreetName = "street", PersonId = john.Id};
+            john.Address = address;
+            context.Addresses.Add(address);
+            context.SaveChanges();
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? AddressId { get; set; }
+        public virtual Address Address { get; set; }
+    }
+
+    public class Address
+    {
+        public int Id { get; set; }
+        public string StreetName { get; set; }
+        public int PersonId { get; set; }
+        public virtual Person Person { get; set; }
+    }
+
+    public class PeopleContext : DbContext
+    {
+        public IDbSet<Person> Persons { get; set; }
+        public IDbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasOptional(x => x.Address).WithMany()
+                .HasForeignKey(x => x.AddressId);
+
+            modelBuilder.Entity<Address>()
+                .HasRequired(x => x.Person).WithMany()
+                .HasForeignKey(x => x.PersonId).WillCascadeOnDelete(false);
+        }
+    }
+}
